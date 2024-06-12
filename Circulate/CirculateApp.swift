2bbfb11841be7fa7f401ad6b5ae61d789fbc5db6
Priyank Shankar <shankarpriyank312002@gmail.com>
2024-06-12 20:35:25 +0530

    Initial Commit

---
+++ Circulate/CirculateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CirculateApp.swift
+//  Circulate
+//
+//  Created by Priyank Shankar  on 12/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CirculateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
